d4b102bd92fbfeca8808e95fd683d8ecffa2db34
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2024-02-19 20:19:44 +0100

    Add files via upload

---
+++ indexA001/myscript.js
@@ -0,0 +1,8 @@
+/*
+Lien : https://www.youtube.com/watch?v=c7H0-88ID78&list=PLCC34OHNcOtoL9GBolQKRq9RiA6PxSqMd
+Cours : Intro To Javascript Programming! - Javascript Programming 1
+
+Date : 19-02-24
+*/
+
+document.getElementById("demo").innerHTML = "My Name is Bob...";
