3a436dd1d5b2e461cbbca92a7560c9ffe851f82d
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2024-02-21 06:39:56 +0100

    Add files via upload

---
+++ indexA003_Arithmetique/myscript.js
@@ -0,0 +1,18 @@
+/*
+Lien : https://www.youtube.com/watch?v=MqrRrS_furc&list=PLCC34OHNcOtoL9GBolQKRq9RiA6PxSqMd&index=3
+Cours : All About Math - Javascript Programming 3
+
+Dans ce cours on apprend à utiliser les mesures arithmétiques
+Quand on fait une division par 0, le resultat est "Infinity"
+Lorsqu'on fait une division, il n'y a pas de limite sur les décimales
+
+Mesures arithmétiques : 
+Addition : + 
+Soustraction : - 
+Multiplication : * 
+Division : / 
+Puissance : ** 
+Modulo : % 
+
+Date : 21-02-24
+*/
