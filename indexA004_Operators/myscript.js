5cee50eb8eabdb9c6239dccae26aa81b47ad09eb
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2024-02-22 06:42:01 +0100

    Add files via upload

---
+++ indexA004_Operators/myscript.js
@@ -0,0 +1,14 @@
+/*
+Lien : https://www.youtube.com/watch?v=U5QznHNWK0g&list=PLCC34OHNcOtoL9GBolQKRq9RiA6PxSqMd&index=4
+Cours : Assignment Operators - Javascript Programming 4
+
+Dans ce tuto on apprend les opérateurs par incrémentation : 
+Incrémentation par addition : += 
+Incrémentation par soustraction : -= 
+Incrémentation par multiplication : *= 
+Incrémentation par division : /= 
+Incrémentation par puissance : **= 
+Incrémentation par modulo : %= 
+
+Date : 22-02/24
+*/
