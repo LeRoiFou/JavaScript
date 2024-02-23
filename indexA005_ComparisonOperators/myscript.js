62267ebac77a660fea73414e2147861f507a57d3
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2024-02-23 07:32:16 +0100

    Add files via upload

---
+++ indexA005_ComparisonOperators/myscript.js
@@ -0,0 +1,18 @@
+/*
+Lien : https://www.youtube.com/watch?v=Mx9tjxdUrQk&list=PLCC34OHNcOtoL9GBolQKRq9RiA6PxSqMd&index=5
+Cours : Comparison Operators - Javascript Programming 5
+
+Dans ce cours on apprend à utiliser les opérateurs de comparaison, avec
+pour résultat un booléen :
+Egal : ==
+Différent de : != 
+Supérieure à > 
+Supérieure ou égal à >= 
+Inférieur à <
+Inférieur ou égal à <= 
+Egal à, avec le même type : === 
+Différent de, avec un type différent : !==
+
+Date : 23-02-24
+Éditeur : Laurent Reynaud
+*/
