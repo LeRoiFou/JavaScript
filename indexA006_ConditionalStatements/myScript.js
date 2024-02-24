3c936fbf6c75136c1a32fcf835911014a886c8b5
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2024-02-24 11:36:47 +0100

    Add files via upload

---
+++ indexA006_ConditionalStatements/myScript.js
@@ -0,0 +1,15 @@
+/*
+Lien : https://www.youtube.com/watch?v=mZ2ab7KD_GU&list=PLCC34OHNcOtoL9GBolQKRq9RiA6PxSqMd&index=6
+Cours : Conditional Statements
+
+Syntaxe pour les conditions (idem que pour Java):
+if(condition) {
+    résultat
+} else if (condition){
+    résultat
+} else {
+    résultat
+}
+
+Date : 24-02-24
+*/
