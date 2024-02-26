2456298b760e033769c3fee07808d5642cb0a50f
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2024-02-26 06:22:20 +0100

    Add files via upload

---
+++ indexA007_LogicalOperators/myScript.js
@@ -0,0 +1,10 @@
+/*
+Lien : https://www.youtube.com/watch?v=LyDS-G3TWPw&list=PLCC34OHNcOtoL9GBolQKRq9RiA6PxSqMd&index=7
+Cours : Logical Operators - Javascript Programming 7
+
+Les opérateurs logiques (à essayer...):
+- AND &&
+- OR ||
+
+Date : 26-02-24
+*/
