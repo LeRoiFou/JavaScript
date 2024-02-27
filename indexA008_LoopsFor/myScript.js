4d31487ce13b3c36af45ad15a74fa86c21e70267
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2024-02-27 05:25:14 +0100

    Add files via upload

---
+++ indexA008_LoopsFor/myScript.js
@@ -0,0 +1,11 @@
+/*
+Lien : https://www.youtube.com/watch?v=8d8SC9t6Cjk&list=PLCC34OHNcOtoL9GBolQKRq9RiA6PxSqMd&index=8
+Cours : For Loops - Javascript Programming 8
+
+Les boucles for (même script que Java) :
+
+for (début; fin; incrémentation){
+    script}
+
+Date : 27-02-24
+*/
