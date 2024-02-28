97697d33b6a6ee77e87c90285ee3bc9cef12f451
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2024-02-28 06:13:13 +0100

    Add files via upload

---
+++ indexA009_LoopsWhile/myScript.js
@@ -0,0 +1,16 @@
+/*
+Lien : https://www.youtube.com/watch?v=r4yJqg9QhRc&list=PLCC34OHNcOtoL9GBolQKRq9RiA6PxSqMd&index=9
+Cours : While Loops - Javascript Programming 9
+
+Syntaxe pour while :
+while (condition) {
+                
+            }
+
+Syntaxe pour do while :
+do {
+                
+            } while (condition);
+
+Date : 28-02-24
+*/
