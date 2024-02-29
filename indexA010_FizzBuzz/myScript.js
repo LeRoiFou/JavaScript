6d021048b09b50f83b23b6f0733759388b9fd8ff
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2024-02-29 05:48:20 +0100

    Add files via upload

---
+++ indexA010_FizzBuzz/myScript.js
@@ -0,0 +1,10 @@
+/*
+Lien : https://www.youtube.com/watch?v=aYEemVhDfT4&list=PLCC34OHNcOtoL9GBolQKRq9RiA6PxSqMd&index=10
+Cours : FizzBuzz!! - Javascript Programming 10
+
+Dans ce cours on apprend à afficher un texte si la valeur est divisible
+par 3 ou par 5
+
+Date : 29-02-24
+Éditeur : Laurent Reynaud
+*/
