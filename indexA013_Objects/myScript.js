a6aea1e0cb20ef148876c83e652a35637b819162
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2024-03-04 07:55:56 +0100

    Add files via upload

---
+++ indexA013_Objects/myScript.js
@@ -0,0 +1,8 @@
+/*
+Lien : https://www.youtube.com/watch?v=x0vjmZZ1VZs&list=PLCC34OHNcOtoL9GBolQKRq9RiA6PxSqMd&index=13
+Cours : Objects (Not Dictionaries) - Javascript Programming 13
+
+Objects n'est pas de la POO, mais un dictionnaire...
+
+Date : 04-03-24
+*/
