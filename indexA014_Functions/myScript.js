a627176e34fb2721bf4fb76b454f8b067eb3e15f
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2024-03-05 06:35:45 +0100

    Add files via upload

---
+++ indexA014_Functions/myScript.js
@@ -0,0 +1,13 @@
+/*
+Lien : https://www.youtube.com/watch?v=UtINL7t2mXU&list=PLCC34OHNcOtoL9GBolQKRq9RiA6PxSqMd&index=14
+Cours : Mastering Javascript Functions - Javascript Programming 14
+
+Dans ce cours, on apprend à utiliser les fonctions selon différentes manières
+
+Date : 05-03-24
+*/
+
+// Définition d'une fonction avec paramètre et avec avec retour
+function hello4(firstname, lastname) {
+  return "Hello " + firstname + " " + lastname + "!<br/>";
+}
