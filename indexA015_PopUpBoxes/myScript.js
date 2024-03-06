20fdbe8e3288c4660818c90f6ac42ba30319c61f
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2024-03-06 06:02:08 +0100

    Add files via upload

---
+++ indexA015_PopUpBoxes/myScript.js
@@ -0,0 +1,8 @@
+/*
+Lien : https://www.youtube.com/watch?v=fjUbuYuYPvE&list=PLCC34OHNcOtoL9GBolQKRq9RiA6PxSqMd&index=15
+Cours : Popup Boxes - Javascript Programming 15
+
+Dans ce cours, on apprend à utiliser une boîte de texte qui s'affiche...
+
+Date : 06-03-24
+*/
