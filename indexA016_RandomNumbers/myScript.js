581eaa1d407dca715ff0f7e1a5e9b78a2ff6bd00
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2024-03-07 06:48:55 +0100

    Add files via upload

---
+++ indexA016_RandomNumbers/myScript.js
@@ -0,0 +1,6 @@
+/*
+Lien : https://www.youtube.com/watch?v=eu9nPTI14aU&list=PLCC34OHNcOtoL9GBolQKRq9RiA6PxSqMd&index=16
+Cours : Why Are Random Numbers So Hard?! - Javascript Programming 16
+
+Date : 07-03-24
+*/
