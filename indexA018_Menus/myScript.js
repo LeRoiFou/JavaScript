2010c1d559ac70167ac9cd4ce3921797646ffc4d
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2024-03-09 09:19:11 +0100

    Add files via upload

---
+++ indexA018_Menus/myScript.js
@@ -0,0 +1,14 @@
+/*
+Lien : https://www.youtube.com/watch?v=PLU4N7a84gk&list=PLCC34OHNcOtoL9GBolQKRq9RiA6PxSqMd&index=18
+Cours : Radio Buttons and Drop Down Menus - Javascript Programming 18
+
+Les scripts HTML pour insérer les composants ont été directement copiés
+depuis le site https://getbootstrap.com/ (URL mis en favoris @)
+
+Lien entre HTML et JavaScript :
+-> Pour chaque script HTML d'un composant, il y a un id (comme pour dash de python)
+-> cet id est récupéré par le script JavaScript par initialisation dans une
+variable
+
+Date : 09-03-24
+*/
