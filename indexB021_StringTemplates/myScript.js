25a7cb245af10604369b90f4d098ae917aa1110b
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2024-03-19 06:23:42 +0100

    Add files via upload

---
+++ indexB021_StringTemplates/myScript.js
@@ -0,0 +1,13 @@
+/*
+Lien : https://www.youtube.com/watch?v=fzpc6yKJCnc
+Cours : String Templates and Interpolation - Javascript Programming 21
+
+Dans ce cours on apprend à utiliser le caractère backtick ` qui se trouve sur
+la touche 7 du clavier : ALT GR + touche 7 pour effectuer des string templates
+(équivalent au f string de python)
+
+Syntaxe :
+`texte ${variable}`
+
+Date : 19-03-24
+*/
