dfd3f28f532f9d6e8f7f50be6e1bd0e47af522be
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2024-03-22 14:29:36 +0100

    Add files via upload

---
+++ indexB004_TypesDonnees/myScript.js
@@ -0,0 +1,23 @@
+/*
+Lien : https://www.youtube.com/watch?v=Ew7KG2j8eII
+Cours : Types de données
+
+00:40:50
+Date : 22-03-24
+*/
+
+let myString = "Je suis une chaîne de caractère";
+let myNumber = 24;
+let myBoolean = true;
+
+// Distinction d'un tableau [] et d'un objet {}
+let myArray = ["je", "suis", 47, true]; // Tableau
+let myObject = {
+  prenom: "Erin",
+  age: 33,
+  ville: "Los Angeles",
+};
+
+// Possibilité de déclarer une variable non assignée (comme pour Java)
+let typeNonAssigne;
+typeNonDeclare = "coucou";
