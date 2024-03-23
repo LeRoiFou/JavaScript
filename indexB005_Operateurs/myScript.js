55a1289dcdfe254b906134d7420cacdd95a51326
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2024-03-23 08:46:40 +0100

    Add files via upload

---
+++ indexB005_Operateurs/myScript.js
@@ -0,0 +1,34 @@
+/*
+Lien : https://www.youtube.com/watch?v=Ew7KG2j8eII
+Cours : Les opérateurs
+
+00:46:55
+
+Date : 23-03-24
+*/
+
+/*Les opérateurs arithmétiques*/
+console.log("Les opérateurs arithmétiques");
+
+console.log(4 + 5);
+console.log(4 - 5);
+console.log(4 * 5);
+console.log(4 / 5);
+console.log(4 ** 5);
+
+/*------------------------------------------*/
+console.log("\n");
+/*------------------------------------------*/
+
+/*Les opérations d'affectation*/
+console.log("Les opérateurs d'affectation");
+
+let total = 0;
+
+// Décrémentation de - 1
+total--;
+console.log(total);
+
+// Incrémentation de 5
+total += 5;
+console.log(total);
