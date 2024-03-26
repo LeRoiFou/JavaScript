e28baec43fc0784a6f865c8d4cce20ab130eaad3
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2024-03-26 06:18:34 +0100

    Add files via upload

---
+++ indexA022_Switch/myScript.js
@@ -0,0 +1,8 @@
+/*
+Lien : https://www.youtube.com/watch?v=7_JXXbWw4dc
+Cours : Switch Statements - Javascript Programming 22
+
+Cours sur la fonction switch() similaire au langage Java
+
+Date : 26-03-24
+*/
