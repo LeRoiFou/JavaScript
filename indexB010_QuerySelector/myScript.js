914678855885a19cf88664f6d7a0bd0587978b61
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2024-04-02 09:17:18 +0200

    Add files via upload

---
+++ indexB010_QuerySelector/myScript.js
@@ -0,0 +1,18 @@
+/*
+Lien : https://www.youtube.com/watch?v=WHodDEhcu-I
+Cours : Query Selector
+
+Les sélecteurs à partir de la page @ :
+Utilisation à partir du menu Console dans la page @ -> inspecter
+Saisi avec l'instruction document.querySelector("instruction")
+
+00:12:10
+Date : 02-04-24
+*/
+
+// Sélection d'une balise affichée en couleur jaune sur la page @
+// document.querySelector("h4").style.background = "yellow";
+
+// Sélection d'une balise instanciée dans une variable
+// const baliseHTML = document.querySelector("h4");
+// console.log((baliseHTML.style.background = "yellow"));
