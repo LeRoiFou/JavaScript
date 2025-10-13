939a166ee6f1cfb6534193804b5ac706ec69473e
LeRoiFou <lrcompta@gmail.com>
2025-10-13 16:32:53 +0200

    cookie lesson updated

--- indexA052.js
+++ indexA052.js
@@ -34,4 +34,58 @@ document.cookie = "prenom1=John; expires=Sun, 12 Oct 2005 10:00:00 UTC; path=/";
 let c3 = document.cookie;
 document.getElementById("cook3").innerHTML = c3;
 
-/* 00:07:55 */
+/* -----------------------------------------------------------------------
+Fonctions pour automatiser la création / la lecture et la suppression d'un cookie
+----------------------------------------------------------------------- */
+
+function creerCookie(nom, valeur, jour) {
+  // Si les jours ont bien été définis...
+  if (jour) {
+    // On créé un objet date stockant la date actuelle
+    let date = new Date();
+    // On définit la date d'expiration du cookie
+    date.setTime(date.getTime() + jour * 24 * 60 * 60 * 1_000);
+    // On met la date au bon format pour un cookie
+    let exp = "; expires=" + date.toGMTString();
+  }
+  // Si les jours n'ont pas été définis, pas besoin de calcul
+  else {
+    let exp = "";
+  }
+  document.cookie = nom + "=" + valeur + exp + ";path=/";
+}
+
+function lireCookie(nom) {
+  // On récupère le nom du cooke et le signe '='
+  let nomEtEgale = nom + "=";
+  // On récupère tous les cookies dans un tableau
+  let cTableau = document.cookie.split(";");
+  // Parcourir le tableau créé
+  for (let i = 0; i < c.cTableau.length; i++) {
+    // on récupère tous les noms et valeurs des cookies
+    let c = cTableau[i];
+    // On supprime les espaces potentiels dans c jusqu'à tomber sur le nom d'un cookie
+    while (c.charAt(0) == " ") {
+      c = c.substring(1, c.length);
+    }
+    if (c.indexOf(nomEtEgale) == 0) {
+      return c.substring(nomEtEgale.length, c.length);
+    }
+  }
+  // Si le nom du cookie n'est pas trouvé, c'est qu'il n'existe pas
+  return null;
+}
+
+function supprimerCookie(nom) {
+  // On donne le nom du cookie à supprimer, puis on utilise creerCookie()
+  // pour indiquer une date d'expiration passée
+  creerCookie(nom, "", -1);
+}
+
+// On crée deux cookies
+creerCookie("Cookie1", "Je suis le premier cookie", 7);
+creerCookie("Cookie2", "Je suis un autre cookie", 7);
+
+let affiche = document.getElementById("cook4");
+affiche.innerHTML = "Valeur : " + lireCookie("Cookie1");
+supprimerCookie("Cookie1");
