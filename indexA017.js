ff8beae13eb486c83237cc51fbca84646e73bd13
LeRoiFou <lrcompta@gmail.com>
2025-10-23 07:55:05 +0200

    indexA017 updated

--- indexA017.js
+++ indexA017.js
@@ -1,17 +1,5 @@
 /* -------------------------------------------------------------
-1ère méthode  - new Object() : DECONSEILLEE !!!
-------------------------------------------------------------- */
-
-let objet_cree = new Object();
-
-objet_cree.prenom = "Pierre";
-objet_cree.nom = "Giraud";
-objet_cree.age = 25;
-
-console.log(objet_cree.prenom);
-
-/* -------------------------------------------------------------
-2ème méthode - Objet littéral {} : POUR DES OBJETS SIMPLES
+1ère méthode - Objet littéral {} : POUR DES OBJETS SIMPLES
 ------------------------------------------------------------- */
 
 const moi = {
@@ -27,7 +15,7 @@ const moi = {
 console.log(moi.identite());
 
 /* -------------------------------------------------------------
-3ème méthode  - Constructeurs : YES ! YES ! YES !
+2ème méthode  - Constructeurs : YES ! YES ! YES !
 ------------------------------------------------------------- */
 
 class IdentiteNouvelle {
@@ -41,3 +29,7 @@ class IdentiteNouvelle {
 let lena = new IdentiteNouvelle("Lena", "Anderson", 24);
 let erin = new IdentiteNouvelle("Erin", "Moriarty", 30);
 console.log(`Age de Lena : ${lena.age} \nAge d'Erin : ${erin.age}`);
+
+const test = (a, b) => {
+  return a * b;
+};
